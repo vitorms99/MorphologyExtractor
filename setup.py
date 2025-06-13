05dcf280576f5f55eef12b5d8c4f81ec7e4c3703
Vitor Medeiros Sampaio <vitorms999@gmail.com>
2025-06-13 19:58:47 -0400

    Complete 1.0.2 version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ ext_modules = cythonize(
 
 setup(
     name="galmex",
-    version="1.0.1",
+    version="1.0.2",
     description="Python package for measuring non-parametric morphological indices of galaxies",
     author="V. M. Sampaio",
     author_email="vitorms999@gmail.com",
